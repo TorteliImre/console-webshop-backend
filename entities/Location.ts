72d34b2c4ca6291cbeab915caa63c876e95d3586
timre13 <itorteli13@gmail.com>
2024-10-08 09:56:38 +0200

    Location: Use id instead of ZIP

--- entities/Location.ts
+++ entities/Location.ts
@@ -1,12 +1,15 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Advert } from "./Advert";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Advert } from './Advert';
 
-@Entity("location", { schema: "console-webshop" })
+@Entity('location', { schema: 'console-webshop' })
 export class Location {
-  @PrimaryGeneratedColumn({ type: "int", name: "zip" })
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('int', { name: 'zip' })
   zip: number;
 
-  @Column("varchar", { name: "name", length: 100 })
+  @Column('varchar', { name: 'name', length: 100 })
   name: string;
 
   @OneToMany(() => Advert, (advert) => advert.locationZip2)
