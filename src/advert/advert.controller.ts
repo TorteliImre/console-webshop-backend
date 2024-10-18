e298aa98f85291b1c9562129b876222ee8c4a2f3
timre13 <itorteli13@gmail.com>
2024-10-18 12:20:22 +0200

    Implement getting adverts

--- src/advert/advert.controller.ts
+++ src/advert/advert.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Post, Query, Request, UseGuards } from '@nestjs/common';
-import { CreateAdvertDto } from './advert.do';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  AddPictureToAdvertDto,
+  CreateAdvertDto as AdvertDto,
+} from './advert.do';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { AdvertService } from './advert.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('advert')
 export class AdvertController {
-  constructor(private readonly filtersService: AdvertService) {}
+  constructor(private readonly advertsService: AdvertService) {}
+
+  @Get(':id')
+  @ApiOperation({ tags: ['adverts'] })
+  @ApiOkResponse({ type: AdvertDto })
+  async getAdvert(@Param('id') id: number) {
+    const found = await this.advertsService.findById(id);
+    if (!found) throw new BadRequestException('No such advert');
+    return found;
+  }
 
   @Post('createAdvert')
   @ApiOperation({ tags: ['adverts'] })
--- src/advert/advert.service.ts
+++ src/advert/advert.service.ts
@@ -1,5 +1,9 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAdvertDto } from './advert.do';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AddPictureToAdvertDto, CreateAdvertDto } from './advert.do';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Advert } from 'entities/Advert';
 import { Repository } from 'typeorm';
@@ -9,10 +13,38 @@ export class AdvertService {
   @InjectRepository(Advert)
   private advertRepository: Repository<Advert>;
 
+  async findById(id: number) {
+    return await this.advertRepository.findOneBy({ id });
+  }
+
   async createAdvert(dto: CreateAdvertDto, userId: number) {
     let toInsert = dto.toEntity();
     toInsert.ownerId = userId;
     let result = await this.advertRepository.insert(toInsert);
+    return result.identifiers[0].id;
+  }
+
+  async addPictureToAdvert(dto: AddPictureToAdvertDto, userId: number) {
+    const advertOwner: number | null = (
+      await this.advertRepository.findOne({
+        where: { id: dto.advertId },
+        select: { ownerId: true },
+      })
+    )?.ownerId;
+    if (advertOwner == null) {
+      throw new NotFoundException('No such advertisement');
+    }
+    if (advertOwner != userId) {
+      throw new UnauthorizedException(
+        'Cannot modify advertisement of another user',
+      );
+    }
+
+    const toInsert = new AdvertPics();
+    toInsert.advertId = dto.advertId;
+    toInsert.data = Buffer.from(dto.picture, 'base64');
+    toInsert.description = dto.description;
+    const result = await this.advertPicsRepository.insert(toInsert);
     return result.identifiers[0].id;
   }
 }
